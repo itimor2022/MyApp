1ea4a55bad29ed4635a39be4867506c0d43ca369
tony <tony@gmail.com>
2026-04-26 11:19:55 +0700

    包名修复

--- app/src/androidTest/java/com/obs/yl/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/obs/yl/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.bilibili.btc101
+package com.obs.yl
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bilibili.btc101", appContext.packageName)
+        assertEquals("com.obs.yl", appContext.packageName)
     }
 }
